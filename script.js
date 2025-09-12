1d5aecb564948af5e2c459caaedb68d0d93dd5bd
amixmotion-ash <amixmotion@gmail.com>
2025-09-12 13:11:41 +0100

    Force update to new design

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 const navToggle = document.querySelector('.nav-toggle');
-const nav = document.querySelector('nav');
+const mainNav = document.querySelector('.main-nav');
 
 navToggle.addEventListener('click', () => {
-    nav.classList.toggle('nav-open');
-    // We also need to toggle the class on the button itself for the 'X' animation
-    navToggle.classList.toggle('nav-open'); 
+    mainNav.classList.toggle('nav-open');
 });
